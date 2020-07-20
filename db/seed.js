43c5d039574cfe0f6145f4f789ca70edd740436f
Nathan Martel <nathanmartel@gmail.com>
2020-07-19 22:05:31 -0700

    Added seed file

---
+++ db/seed.js
@@ -0,0 +1,28 @@
+const User = require('../lib/models/User');
+const Address = require('../lib/models/Address');
+const Campaign = require('../lib/models/Campaign');
+const chance = require('chance').Chance();
+
+module.exports = async({ usersToCreate = 1, addressesToCreate = 1, campaignsToCreate = 1 } = {}) => {
+  const users = await User.create([...Array(usersToCreate)].map(() => ({
+    email: chance.email(),
+    password: chance.animal()
+  })));
+
+  const addresses = await Address.create([...Array(addressesToCreate)].map(() => ({
+    street1: chance.address(),
+    city: chance.city(),
+    state: chance.state(),
+    zip: chance.zip()
+  })));
+
+  const campaigns = await Campaign.create([...Array(campaignsToCreate)].map(() => ({
+    author: chance.pickone(users)._id,
+    title: chance.sentence({ words: 3 }),
+    description: chance.sentence(),
+    recipient: chance.name(),
+    address: chance.pickone(addresses)._id,
+    defaultPostcard: 'postcard ID to come',
+  })));
+
+};
